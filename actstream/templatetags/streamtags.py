32c0393a85ee27913395b4428139ca32b168e450
luterien <uguryoruk26@gmail.com>
2013-07-03 13:26:51 +0300

    comment fix

--- actstream/templatetags/streamtags.py
+++ actstream/templatetags/streamtags.py
@@ -5,14 +5,14 @@ register = template.Library()
 
 def get_actions_for_actor():
     """
-	Returns a list of actions performed by the specified actor
+	Returns a list of actions performed by this particular actor
     """
     pass
 
 
 def get_actions_for_target():
     """
-        Returns a list of actions performed on the specified target object
+        Returns a list of actions performed on this particular target
     """
     pass
 
