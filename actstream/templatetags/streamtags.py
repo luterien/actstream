fd5be641e404164b6cea40c448e47bb3e81881c3
luterien <uguryoruk26@gmail.com>
2013-07-03 13:15:00 +0300

    template tags init

---
+++ actstream/templatetags/streamtags.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+
+def get_actions_for_actor():
+    """
+	Returns a list of actions performed by the specified actor
+    """
+    pass
+
+
+def get_actions_for_target():
+    """
+        Returns a list of actions performed on the specified target object
+    """
+    pass
+
+
